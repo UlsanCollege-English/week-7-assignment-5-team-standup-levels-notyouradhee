8aec0cfa337720e7043c28805cb5e245b16cd58b
notyouradhee <guptaadit2004@gmail.com>
2025-10-26 12:03:11 +0545

    levels working

--- src/levels.py
+++ src/levels.py
@@ -8,12 +8,77 @@ class Node:
 
 def level_order(root):
     # TODO: return list of lists
-    pass
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        
+        for _ in range(level_size):
+            node = queue.popleft()
+            current_level.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+                
+        result.append(current_level)
+    
+    return result
 
 def zigzag_level_order(root):
     # TODO
-    pass
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    left_to_right = True
+    
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        
+        for _ in range(level_size):
+            node = queue.popleft()
+            current_level.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        if left_to_right:
+            result.append(current_level)
+        else:
+            result.append(current_level[::-1])
+            
+        left_to_right = not left_to_right
+    
+    return result
 
 def right_side_view(root):
     # TODO
-    pass
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    
+    while queue:
+        level_size = len(queue)
+        
+        for i in range(level_size):
+            node = queue.popleft()
+            
+            if i == level_size - 1:
+                result.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    
+    return result
